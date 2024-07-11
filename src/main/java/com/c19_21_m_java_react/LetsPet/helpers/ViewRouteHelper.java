2a89dae80767d15f6b780925cf716c2417958104
martina_b_gramajo <martina.b.gramajo@gmail.com>
2024-07-11 14:13:41 -0300

    Estructura Spring inicial

---
+++ src/main/java/com/c19_21_m_java_react/LetsPet/helpers/ViewRouteHelper.java
@@ -0,0 +1,14 @@
+package com.c19_21_m_java_react.LetsPet.helpers;
+
+public class ViewRouteHelper {
+
+	// ----VISTAS----//
+
+	// HOME
+	public final static String INDEX = "home/index";
+
+	
+	// REDIRECCIONES
+	public final static String ROUTE = "/index";
+
+}
