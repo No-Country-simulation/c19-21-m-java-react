26e19bf0413c05d611d3eaf1c3cf4a160d2a4c55
emmaaavd <emmaaavd@gmail.com>
2024-07-20 00:20:07 +0000

    Eliminacion de codigo css no utilizado

--- frontend/LetsPet/src/components/NavBar/NavBar.jsx
+++ frontend/LetsPet/src/components/NavBar/NavBar.jsx
@@ -2,24 +2,15 @@ import React from "react";
 import petsLogo from './assets/petsLogo.png'
 import { Link } from "react-router-dom";
 import { useAuth0 } from '@auth0/auth0-react';
-import { css } from "@emotion/react";
 
 
-const headerNav = css({
-    height: '10vh',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '0 2rem',
-    color: '#f2ddb2',
-})
 
 const NavBar = () => {
 
     const { isAuthenticated } = useAuth0();
 
     return (
-        <headerNav className="navbar" id="sidebar">
+        <div className="navbar" id="sidebar">
             <Link to='/'><img src={petsLogo} alt="Let's Pet Logo" className="letspet-logo" /></Link>
             <ul style={{display: 'flex', width: '90vw', alignItems: 'center', height: '10vh', margin: '0'}} className="menu">
                 <Link to='/servicios'>Servicios</Link>
@@ -38,7 +29,7 @@ const NavBar = () => {
                         )
                     }
             </ul>
-        </headerNav>
+        </div>
     );
 }
 
