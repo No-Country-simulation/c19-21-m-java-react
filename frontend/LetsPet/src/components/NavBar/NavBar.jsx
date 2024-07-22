6fda06b7fbbf7d861f036dde4196282cea6ae3cb
Emmanuel van Dyk <93367648+emmanuel-vandyk@users.noreply.github.com>
2024-07-22 12:08:24 +0000

    Pending changes exported from your codespace

--- frontend/LetsPet/src/components/NavBar/NavBar.jsx
+++ frontend/LetsPet/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import petsLogo from './assets/petsLogo.png'
+import petsLogo from './assets/letspet-png.png'
 import { Link } from "react-router-dom";
 import { useAuth0 } from '@auth0/auth0-react';
 
--- frontend/LetsPet/src/pages/home/Home.jsx
+++ frontend/LetsPet/src/pages/home/Home.jsx
@@ -1,19 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
 import './Home.css'
-/* import reactLogo from './assets/happy-dogs.jpg' */
 import viteLogo from './assets/happy-dog.jpg'
 import { Link } from 'react-router-dom'
 
-function Home () {
+function Home() {
     return (
-        <div className='home'>
-            <h1>¡En Let's Pet cuidamos y paseamos a tu mascota!</h1>
+        <div className="home">
+            <h1>Qué necesitas?</h1>
             <form className="form">
-                <div className="div-form">
                     <Link to="#" className="logo">
                         <img src={viteLogo} className="logo react" alt="React logo" />
                     </Link>
+                <div className="div-form">
                     <div className="div-btns">
-                    <legend style={{textAlign: 'start', padding: '1rem 0 0 .5rem', color: 'black', fontWeight: 'bold', fontSize: '20px', margin: '0'}}>Que necesitas?</legend>
                         <button type="submit">Encontrar cuidador</button>
                         <button type="submit">Encontrar paseador</button>
                         <input type="address" name="address" id="address" placeholder="Lugar" />
@@ -22,21 +21,21 @@ function Home () {
                             <input type="checkbox" name="checkbox" id="checkbox-pequeño" placeholder="Pequeño" />
                             <label htmlFor="checkbox-pequeño" id="checkbox-pequeño">Pequeño</label>
                         </div>
-                       <div className="checkbox">
+                        <div className="checkbox">
                             <input type="checkbox" name="checkbox" id="checkbox-mediano" placeholder="Mediano" />
                             <label htmlFor="checkbox-mediano" id="checkbox-mediano">Mediano</label>
-                       </div>
-                       <div className="checkbox">
+                        </div>
+                        <div className="checkbox">
                             <input type="checkbox" name="checkbox" id="checkbox-grande" placeholder="Grande" />
                             <label htmlFor="checkbox-grande" id="checkbox-grande">Grande</label>
                             <button id="search" type="submit">Buscar</button>
-                       </div>
+                        </div>
                     </div>
                 </div>
             </form>
 
             <section>
-                <h2 style={{color: 'black', marginTop: '10%', padding: 0, marginBottom: 0, fontSize: '40px'}}>Cómo funciona?</h2>
+                <h2 style={{ color: 'black', marginTop: '10%', padding: 0, marginBottom: 0, fontSize: '40px' }}>Cómo funciona?</h2>
                 <div className="container">
                 </div>
             </section>
