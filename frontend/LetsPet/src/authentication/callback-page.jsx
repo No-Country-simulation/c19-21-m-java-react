f63ef6ba61f80eebbf52ac666e7d8a7c70b517b7
Emmanuel van Dyk <93367648+emmanuel-vandyk@users.noreply.github.com>
2024-10-03 21:20:23 -0300

    Delete frontend directory

--- frontend/LetsPet/src/authentication/callback-page.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const CallbackPage = () => {
-  return (
-    <div className="page-layout">
-      <div className="page-layout__content" />
-    </div>
-  );
-};
